fbfb448b3e4b69a07213af2b217e91c18be2e690
Ashwini Suresh <ashu04906@gmail.com>
2024-08-27 15:13:59 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
-
+import { BrowserRouter, Route, Routes } from'react-router-dom';
+import AdminLogin from './Pages/AdminLogin/AdminLogin';
+import Dashboard from './Pages/Dashboard/Dashboard';
 function App() {
+  
+  
   return (
-    <div className="App">
-      <b>Hi welcome..!</b>
-    </div>
+    
+<BrowserRouter>
+<div className="d-flex app">
+   {/* <Sidebar/> */}
+ <div className='col'>
+  <div className='app'>
+  {/* <Navbar/>  */}
+   <Routes>
+     <Route path="/" element={<AdminLogin />} />
+     <Route path="/dashboard" element={<Dashboard />} />
+
+   </Routes>
+   {/* <TimeSlotSelector/> */}
+   </div>
+   </div>
+   </div>
+  
+ </BrowserRouter>
   );
 }
 
